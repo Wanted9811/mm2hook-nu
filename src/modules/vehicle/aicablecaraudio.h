ca68ff4e9025730c2c05f93e5e2bd27990b1d390
Eslam Mahmoud <41547778+Wanted9811@users.noreply.github.com>
2025-09-20 20:22:22 +0300

    Add aiCableCarAudio class

--- src/modules/vehicle/aicablecaraudio.h
+++ src/modules/vehicle/aicablecaraudio.h
@@ -1,16 +1,21 @@
 #pragma once
 #include <modules\vehicle.h>
+#include "aicablecaraudiodata.h"
 
 namespace MM2
 {
     // Forward declarations
-
+    class aiCableCarAudio;
 
     // External declarations
-
+    extern class aiCableCarAudioData;
 
     // Class definitions
-
+    class aiCableCarAudio : public Aud3DObject
+    {
+        aiCableCarAudioData AudioData;
+    };
+    ASSERT_SIZEOF(aiCableCarAudio, 0x94);
 
     // Lua initialization
 
