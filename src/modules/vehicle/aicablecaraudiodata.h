d2807c51e94ab2d4f1f69bf7b9faff6dbea9bb12
Eslam Mahmoud <41547778+Wanted9811@users.noreply.github.com>
2025-09-20 20:21:25 +0300

    Add aiCableCarAudioData class

--- src/modules/vehicle/aicablecaraudiodata.h
+++ src/modules/vehicle/aicablecaraudiodata.h
@@ -4,13 +4,30 @@
 namespace MM2
 {
     // Forward declarations
-
+    class aiCableCarAudioData;
 
     // External declarations
 
 
     // Class definitions
-
+    class aiCableCarAudioData
+    {
+    private:
+        AudSoundBase* GoBellSoundBasePtr;
+        AudSoundBase* StopSoundBasePtr;
+        AudSoundBase* LoopSoundBasePtr;
+        AudSoundBase* StartSoundBasePtr;
+        AudSoundBase* CrossIntersectionSoundBasePtr;
+        AudSoundBase* StreetCableSoundBasePtr;
+        int GoBellSampleIndex;
+        int StopSampleIndex;
+        int LoopSampleIndex;
+        int StartSampleIndex;
+        int StreetCableSampleIndex;
+        int State;
+        int LastState;
+    };
+    ASSERT_SIZEOF(aiCableCarAudioData, 0x34);
 
     // Lua initialization
 
